27569f24bc2de1f37699b4a0763d45ae20918d46
lendle <lendle_tseng@seed.net.tw>
2019-05-11 15:10:42 +0800

    use only most frequently used tags for clustering

--- questions.js
+++ questions.js
@@ -6,6 +6,7 @@ const TfIdf = natural.TfIdf;
 const tfidf = new TfIdf();
 const tokenizer = new natural.WordTokenizer();
 const { Parser } = require('json2csv');
+const mathjs = require('mathjs');
 //let url="https://api.stackexchange.com/2.2/questions?order=desc&sort=votes&min=50&site=stackoverflow&fromdate=1514764800&todate=1546214400";
 let url = "https://api.stackexchange.com/2.2/questions?key=TsELULFShNfv3LtUcuTk4Q((&order=desc&sort=votes&min=10&site=stackoverflow&tagged=javascript&pagesize=100";
 
@@ -56,7 +57,7 @@ function collectTags() {
     let totalTokenCount = 0;
     for (let item of items) {
         let tags = item.tags;
-        
+
         let title = item.title;
         tfidf.addDocument(title);
         let _tokens = tokenizer.tokenize(title);
@@ -87,7 +88,7 @@ function collectTags() {
     }
 
     let filteredTagMap = {};
-    
+
     for (let tag in tagMap) {
         if (tagMap[tag] >= 50) {
             filteredTagMap[tag] = tagMap[tag];
@@ -100,10 +101,10 @@ function collectTags() {
 function processTags() {
     let tags = JSON.parse(fs.readFileSync("tags.json"));
     let filteredTagMap = {};
-    let mapping={};
+    let mapping = {};
     for (let tag in tags) {
         let stemmedTag = natural.PorterStemmer.stem(tag).toLowerCase();
-        mapping[stemmedTag]=tag;
+        mapping[stemmedTag] = tag;
         //let stemmedTag = tag.toLowerCase();
         if (stemmedTag.length > 2 && stemmedTag.match(/[^$\d]/)) {
             //console.log(tag + ":" + stemmedTag);
@@ -115,64 +116,80 @@ function processTags() {
         }
     }
     //console.log(filteredTagMap);
-    let sortedTagArray=[];
-    for(let tag in filteredTagMap){
+    let sortedTagArray = [];
+    for (let tag in filteredTagMap) {
         sortedTagArray.push({
             tag: tag,
             count: filteredTagMap[tag],
             original: mapping[tag]
         });
     }
-    sortedTagArray.sort(function(o1, o2){
-        return (o1.count-o2.count)*-1;
+    sortedTagArray.sort(function (o1, o2) {
+        return (o1.count - o2.count) * -1;
     });
     //console.log(sortedTagArray);
     //console.log(sortedTagArray.length);
     fs.writeFileSync("important_tokens.json", JSON.stringify(sortedTagArray));
+    //filter less important tags
+    let counts = [];
+    for (let tag of sortedTagArray) {
+        if (!isNaN(tag.count)) {
+            counts.push(tag.count);
+        }
+    }
+    let mean = mathjs.mean(counts);
+    let filteredTokens = [];
+    for (let tag of sortedTagArray) {
+        if (!isNaN(tag.count) && tag.count >= mean) {
+            filteredTokens.push(tag);
+        }
+    }
+    console.log(filteredTokens.length);
+    fs.writeFileSync("filteredTokens.json", JSON.stringify(filteredTokens));
 }
 
-function convert2CSV(){
+function convert2CSV() {
     let questions = JSON.parse(fs.readFileSync("javascript_questions.json"));
-    let tagsInfo=JSON.parse(fs.readFileSync("important_tokens.json"));
-    let tags=tagsInfo.map(x=>x.tag);
+    let tagsInfo = JSON.parse(fs.readFileSync("filteredTokens.json"));
+    let tags = tagsInfo.map(x => x.tag);
     console.log(tags);
-    let fields=["index", "title", ...tags];
-    let records=[];
-    let index=0;
-    let outputBuffer="";
+    let fields = ["index", ...tags];
+    let records = [];
+    let index = 0;
+    let outputBuffer = "";
     //have to write out the file gradually to prevent heap overflow
-    fs.writeFileSync("questions.csv", fields.join(",")+"\r\n");
-    for(let question of questions){
-        console.log(question.title+":"+index);
-        let record=[index, ""];
-        let title=question.title;
+    fs.writeFileSync("questions.csv", fields.join(",") + "\r\n");
+    for (let question of questions) {
+        console.log(question.title + ":" + index);
+        let record = [index];
+        let title = question.title;
         let tokens = tokenizer.tokenize(title);
-        let stemmedTokens=tokens.map(x=>natural.PorterStemmer.stem(x).toLowerCase());
-        let stemmedTokensMap={};
-        for(let stemmedToken of stemmedTokens){
-            stemmedTokensMap[stemmedToken]="1";
+        let stemmedTokens = tokens.map(x => natural.PorterStemmer.stem(x).toLowerCase());
+        let stemmedTokensMap = {};
+        for (let stemmedToken of stemmedTokens) {
+            stemmedTokensMap[stemmedToken] = "1";
         }
         //console.log(tags.length);
         //console.log(stemmedTokensMap.constructor);
-        let hitCount=0;
-        for(let tag of tags){
+        let hitCount = 0;
+        for (let tag of tags) {
             //console.log("\t"+tag);
-            if(stemmedTokensMap[tag]=="1"){
+            if (stemmedTokensMap[tag] == "1") {
                 hitCount++;
                 //console.log("\t\t"+tag);
                 record.push(1);
-            }else{
+            } else {
                 record.push(0);
             }
         }
-        console.log("\t"+hitCount);
-        if(outputBuffer.length>0 || index%1000==1){
-            outputBuffer+="\r\n";
+        console.log("\t" + hitCount);
+        if (outputBuffer.length > 0 || index % 1000 == 1) {
+            outputBuffer += "\r\n";
         }
-        outputBuffer+=record.join(",");
-        if(index%1000==0){
+        outputBuffer += record.join(",");
+        if (index % 1000 == 0) {
             fs.appendFileSync("questions.csv", outputBuffer);
-            outputBuffer="";
+            outputBuffer = "";
         }
         index++;
         // let record={};
@@ -199,9 +216,9 @@ function convert2CSV(){
         // delete stemmedTokens;
         // delete stemmedTokensMap;
     }
-    if(outputBuffer.length>0){
+    if (outputBuffer.length > 0) {
         fs.appendFileSync("questions.csv", outputBuffer);
-        outputBuffer="";
+        outputBuffer = "";
     }
     // console.log("converting");
     // const json2csvParser = new Parser({ fields: fields});
@@ -212,4 +229,13 @@ function convert2CSV(){
 //collectTags();
 //processTags();
 //console.log(natural.PorterStemmer.stem("javascript"));
-convert2CSV();
+//convert2CSV();
+
+let json = fs.readFileSync("javascript_questions.json");
+let items = JSON.parse(json);
+for(let i of [933,3368,3458,3612,4075,4911,7063,9706,14841,14924,18818,20058,20390,23621,24379,25596,26570,29627,32209,33106,33153,37401,39144,43595]){
+    console.log(items[i].title);
+    for(let tag of items[i].tags){
+        console.log("\t"+tag);
+    }
+}
